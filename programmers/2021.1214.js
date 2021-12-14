ec748bbbaa59e40a03ea4d64ea0aa1cd6dc87646
hyeokangel <dltkdgur7171@gmail.com>
2021-12-14 13:58:59 +0900

    알고리즘

---
+++ programmers/2021.1214.js
@@ -0,0 +1,42 @@
+// 자연수 뒤집어 배열로 만들기
+// 문제 설명
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+function solution(n) {
+  var arr = n.toString().split("");
+  var answer = [];
+
+  for (var i = arr.length - 1; i >= 0; i--) {
+    answer.push(Number(arr[i]));
+  }
+
+  return answer;
+}
+
+// 나누어 떨어지는 숫자 배열
+// 문제 설명
+// array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+// divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+
+// 제한사항
+// arr은 자연수를 담은 배열입니다.
+// 정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+// divisor는 자연수입니다.
+// array는 길이 1 이상인 배열입니다.
+// 입출력 예
+// arr	divisor	return
+// [5, 9, 7, 10]	5	[5, 10]
+// [2, 36, 1, 3]	1	[1, 2, 3, 36]
+// [3,2,6]	10	[-1]
+
+function solution(arr, divisor) {
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
+}
